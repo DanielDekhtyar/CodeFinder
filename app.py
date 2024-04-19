4f5a8f05f15c2733cfe7456badb4f9f66505f79a
Daniel Dekhtyar <denik2707@gmail.com>
2024-04-19 20:52:38 +0300

    language images updated

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import *
 from flask_session import Session
 
+import os
+
+from backend import api_requests
+
 # Configure application
 app = Flask(__name__)
 
@@ -15,26 +19,11 @@ def search():
     # Retrieve form data
     user_query = request.args.get('q')
     user_query.strip()
-
-    # Construct the GitHub search URL
-    search_query = "https://github.com/search?q=" + user_query
     
-    search_results = [
-        {'title': 'Result 1', 'snippet': 'Description of result 1', 'url': 'https://example.com/result1'},
-        {'title': 'Result 2', 'snippet': 'Description of result 2', 'url': 'https://example.com/result2'},
-        {'title': 'Result 3', 'snippet': 'Description of result 3', 'url': 'https://example.com/result3'},
-        {'title': 'Result 4', 'snippet': 'Description of result 4', 'url': 'https://example.com/result4'},
-        {'title': 'Result 5', 'snippet': 'Description of result 5', 'url': 'https://example.com/result5'},
-        {'title': 'Result 6', 'snippet': 'Description of result 6', 'url': 'https://example.com/result6'},
-        {'title': 'Result 7', 'snippet': 'Description of result 7', 'url': 'https://example.com/result7'},
-        {'title': 'Result 8', 'snippet': 'Description of result 8', 'url': 'https://example.com/result8'},
-        {'title': 'Result 9', 'snippet': 'Description of result 9', 'url': 'https://example.com/result9'},
-        {'title': 'Result 10', 'snippet': 'Description of result 10', 'url': 'https://example.com/result10'},
-        # Add more search results as needed
-    ]
+    search_results, time_it_took, result_count = api_requests.repositories(user_query)
 
     # Redirect to the GitHub search URL
-    return render_template("results_template.html", query=user_query, search_results=search_results)
+    return render_template("results_template.html", query=user_query, search_results=search_results, time_it_took=time_it_took, result_count=result_count, os=os)
 
 
 if __name__ == '__main__':
