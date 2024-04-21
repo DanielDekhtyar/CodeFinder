065842aa827dba7cbf18353867d4bb63b6175ecc
Daniel Dekhtyar <denik2707@gmail.com>
2024-04-22 02:03:04 +0300

    user can type /  to put the search bar in focus

--- static/js/results_template.js
+++ static/js/results_template.js
@@ -3,7 +3,6 @@ logo.addEventListener("click", () => {
     window.location.href = "/";
 })
 
-
 // Functions when you hover over the search icon
 function hover() {
     element = document.getElementById("search-icon");
@@ -14,3 +13,38 @@ function unhover() {
     element = document.getElementById("search-icon");
     element.setAttribute('src', 'static/assets/search_black.png');
 }
+
+// Get the input element
+var searchBar = document.getElementsByClassName("form-control")[0];
+
+// Event listener for when the input box gains focus
+searchBar.addEventListener("focus", function() {
+    // Reset the placeholder text when the input box gains focus
+    this.placeholder = "What are you looking for?";
+});
+
+// Event listener for when the input box loses focus
+searchBar.addEventListener("blur", function() {
+    // Check if the input box is empty
+    if (!this.value.trim()) {
+        // Change the placeholder text
+        this.placeholder = "Type [/] to search";
+        console.log("placeholder changed");
+    }
+});
+
+// Event listener for when the '/' key is pressed
+document.addEventListener("keydown", function(event) {
+    // Check if the '/' key is pressed
+    if (event.key === '/') {
+        // Prevent the default behavior of the '/' key press event
+        event.preventDefault();
+        
+        // Set focus to the input element
+        searchBar.focus();
+        
+        // Set the cursor at the end of the current text
+        var currentTextLength = searchBar.value.length;
+        searchBar.setSelectionRange(currentTextLength, currentTextLength);
+    }
+});
