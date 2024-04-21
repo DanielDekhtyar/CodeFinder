b5852ab55858b63eb68317b94210da30f23777df
Daniel Dekhtyar <denik2707@gmail.com>
2024-04-22 02:14:05 +0300

    error handling in case API limit reached

--- backend/api_requests.py
+++ backend/api_requests.py
@@ -28,25 +28,30 @@ def repositories(user_query, page):
 
     # Get the JSON data from the response
     data:dict = response.json()
-
-    # Create an empty list to store the search results
-    search_results = []
-
-    # Get all the relevant data from the JSON and append it to the search_results list
-    for repo in data.get('items', []):  # Iterate over each item in the 'items' list
-
-        search_results.append({
-            'title': repo['full_name'],
-            'avatar_url': repo['owner']['avatar_url'],
-            'language': repo['language'],
-            'license': repo['license']['name'] if repo.get('license') else None,  # Check if 'license' exists
-            'license_key': repo['license']['key'] if repo.get('license') else None,
-            'description': repo['description'] if repo.get('description') else None,  # Check if 'description' exists
-            'pushed_at': helpers.format_date(repo['pushed_at']),
-            'repo_url': repo['html_url'],
-            'topics' : repo['topics'] if len(repo['topics']) < 8 else repo['topics'][:8]
-        })
     
+    # Check if the API limit was reached
+    if data.get('incomplete_results', 0) == True:
+        search_results = "API limit reached"
+    else:
+        # Create an empty list to store the search results
+        search_results = []
+
+        # Get all the relevant data from the JSON and append it to the search_results list
+        for repo in data.get('items', []):  # Iterate over each item in the 'items' list
+
+            search_results.append({
+                'title': repo['full_name'],
+                'owners_page': repo['owner']['html_url'],
+                'avatar_url': repo['owner']['avatar_url'],
+                'language': repo['language'],
+                'license': repo['license']['name'] if repo.get('license') else None,  # Check if 'license' exists
+                'license_key': repo['license']['key'] if repo.get('license') else None,
+                'description': repo['description'] if repo.get('description') else None,  # Check if 'description' exists
+                'pushed_at': helpers.format_date(repo['pushed_at']),
+                'repo_url': repo['html_url'],
+                'topics' : repo['topics'] if len(repo['topics']) < 5 else repo['topics'][:5]
+            })
+        
     # Calculate how much time it took to get the results
     time_it_took:float = time.time() - start_time
     
