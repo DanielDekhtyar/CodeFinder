7e7e09c9f6f927f3de66443fe622a5e3672e00d7
Daniel Dekhtyar <denik2707@gmail.com>
2024-04-22 02:11:54 +0300

    error handling added

--- app.py
+++ app.py
@@ -21,10 +21,24 @@ def search():
     user_query.strip()
     page = request.args.get('page')
     
-    search_results, time_it_took, result_count = api_requests.repositories(user_query, page)
+    api_response = api_requests.repositories(user_query, page)
+    
+    # Error handling
+    if len(api_response) != 3:
+        return render_template("error.html", query=user_query , text="You broke the search engine", gif="break computer.gif", error=422)
+    
+    # Unpack the API response
+    search_results, time_it_took, result_count = api_response
 
-    # Redirect to the GitHub search URL
-    return render_template("results_template.html", query=user_query, search_results=search_results, time_it_took=time_it_took, result_count=result_count, os=os)
+    # If API limit reached display error message
+    if search_results == "API limit reached":
+        return render_template("error.html", query=user_query , text="You've reached GitHub's API limit :)", gif="break computer.gif", error=422)
+    # If there are no results display error message
+    elif result_count == 0:
+        return render_template("error.html", query=user_query , text="No results found", gif="i-cant-find-anything.gif", error=404)
+    else:
+        # Render the search results
+        return render_template("repo_results.html", query=user_query, search_results=search_results, time_it_took=time_it_took, result_count=result_count, os=os)
 
 
 if __name__ == '__main__':
