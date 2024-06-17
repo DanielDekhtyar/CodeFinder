01664cb6e479c086540318d4fa906c0b5dd31f5b
Daniel Dekhtyar <denik2707@gmail.com>
2024-06-17 11:52:13 +0300

    Powered by Formspree added to Contact Me page

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -33,9 +33,3 @@ function showPopup(message) {
         popup.style.display = 'none';
     }, 5000); // Hide popup after 5 seconds
 }
-
-// When the user clicks on the logo, go back to the home page
-const logo = document.getElementById("logo")
-logo.addEventListener("click", () => {
-    window.location.href = "/";
-})
